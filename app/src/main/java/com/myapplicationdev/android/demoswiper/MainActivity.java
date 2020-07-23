71eb0c90c7c55a3dc2b89215a410ba73d44834df
lowfanghong <18021531@myrp.edu.sg>
2020-07-23 11:52:30 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoswiper/MainActivity.java
@@ -0,0 +1,62 @@
+package com.myapplicationdev.android.demoswiper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnBack, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnBack = findViewById(R.id.buttonback);
+        btnNext = findViewById(R.id.buttonNext);
+
+        vPager = findViewById(R.id.viewpager1);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
+
+
+    }
+}
